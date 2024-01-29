44f037800628e52abe7b87a039ec2ab6986ab891
Martin <martin.rubiano1234@gmail.com>
2024-01-29 16:21:59 -0500

    Finish NavBar section

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import { Navbar , Hero ,Stats ,Bussiness,Billing ,CardDeal ,Testimonials ,Clients ,CTA ,Footer  } from './components'
-import styles from './style' 
+import styles from "./style";
+import { Billing, Bussiness, CardDeal, Clients, CTA, Footer, Navbar, Stats, Testimonials, Hero } from "./components";
 
 const App = () => (
-  <div className='bg-primary w-full overflow-hidden'>
+  <div className="bg-primary w-full overflow-hidden">
     <div className={`${styles.paddingX} ${styles.flexCenter}`}>
       <div className={`${styles.boxWidth}`}>
         <Navbar />
@@ -10,28 +10,24 @@ const App = () => (
     </div>
 
     <div className={`bg-primary ${styles.flexStart}`}>
-      <div className={`${ styles.boxWidth}`}>
+      <div className={`${styles.boxWidth}`}>
         <Hero />
       </div>
     </div>
-
-    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
-      <div className={`${ styles.boxWidth}`}>
+    
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
         <Stats />
         <Bussiness />
-        <Billing /> 
-        <CardDeal /> 
-        <Testimonials /> 
-        <Clients /> 
-        <CTA /> 
-        <Footer /> 
+        <Billing />
+        <CardDeal />
+        <Testimonials />
+        <Clients />
+        <CTA />
+        <Footer />
       </div>
     </div>
-
   </div>
+);
 
- 
-
-)
-
-export default App
+export default App;
